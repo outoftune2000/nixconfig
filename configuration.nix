9aad6319db213af8fd928a8ec2378979f408e2f8
outoftune2000 <athul.nazhiyath@gmail.com>
2025-03-18 17:30:27 +0530

    added added appimage support

--- configuration.nix
+++ configuration.nix
@@ -141,7 +141,17 @@ in
     git
     unstable.go
     pkgs.flatpak
+    pkgs.appimage-run
   ];
+  boot.binfmt.registrations.appimage = {
+	wrapInterpreterInShell = false;
+	interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+	recognitionType = "magic";
+	offset = 0;
+	mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+	magicOrExtension = ''\x7fELF....AI\x02'';
+
+};  
 
   services.flatpak.enable = true;
   xdg.portal.enable = true;
@@ -153,6 +163,7 @@ in
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+
 
   # List services that you want to enable:
 
